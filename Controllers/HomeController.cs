867279e17ed6d0350989c10c968700dd5cd79b07
Richard Garside <729381+NogginBox@users.noreply.github.com>
2013-09-17 15:14:44 +0100

    Displays edit form for content and saves it.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,74 @@
+﻿using System.Web.Mvc;
+using Orchard;
+using Orchard.ContentManagement;
+using Orchard.Localization;
+using Orchard.Themes;
+using Orchard.UI.Notify;
+
+namespace NogginBox.CustomFormsEdit.Controllers
+{
+	[ValidateInput(false), Themed]
+	public class HomeController : Controller, IUpdateModel
+	{
+		private IOrchardServices Services { get; set; }
+		public Localizer T { get; set; }
+
+		public HomeController(IOrchardServices services)
+		{
+			Services = services;
+		}
+
+
+		public ActionResult Edit(int contentId)
+		{
+			var content = Services.ContentManager.Get(contentId);
+			if (content == null) return HttpNotFound();
+
+			// Todo: Check permissions
+			/*if(!Services.Authorizer.Authorize(Permissions.ViewProfiles, user, null)) {
+                return HttpNotFound();
+            }*/
+
+			var shape = Services.ContentManager.BuildEditor(content);
+			return View((object)shape);
+		}
+
+		[HttpPost, ActionName("Edit")]
+		public ActionResult EditContent(int contentId)
+		{
+			var content = Services.ContentManager.Get(contentId);
+			if (content == null) return HttpNotFound();
+
+			/*if (Services.WorkContext.CurrentUser == null) {
+				return HttpNotFound();
+			}*/
+
+            //IUser user = Services.WorkContext.CurrentUser;
+
+            var shape = Services.ContentManager.UpdateEditor(content, this);
+            if (!ModelState.IsValid)
+			{
+				Services.TransactionManager.Cancel();
+				return View("Edit", (object)shape);
+            }
+
+            Services.Notifier.Information(T("Your content has been saved."));
+
+            return RedirectToAction("Edit");
+        }
+
+		#region IUpdateModel Methods
+
+		bool IUpdateModel.TryUpdateModel<TModel>(TModel model, string prefix, string[] includeProperties, string[] excludeProperties)
+		{
+            return TryUpdateModel(model, prefix, includeProperties, excludeProperties);
+        }
+
+        void IUpdateModel.AddModelError(string key, LocalizedString errorMessage)
+		{
+            ModelState.AddModelError(key, errorMessage.ToString());
+        }
+		
+		#endregion
+	}
+}
---
+++ Routes.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+using System.Web.Routing;
+using Orchard.Mvc.Routes;
+
+namespace NogginBox.CustomFormsEdit
+{
+    public class Routes : IRouteProvider
+	{
+		public void GetRoutes(ICollection<RouteDescriptor> routes)
+		{
+			foreach (var routeDescriptor in GetRoutes())
+				routes.Add(routeDescriptor);
+        }
+
+		public IEnumerable<RouteDescriptor> GetRoutes()
+		{
+			return new[]
+			{
+				new RouteDescriptor
+				{
+					Priority = 6,
+					Route = new Route(
+						"Content/Edit/{contentId}",
+						new RouteValueDictionary
+						{
+							{"area", "NogginBox.CustomFormsEdit"},
+							{"controller", "Home"},
+							{"action", "Edit"}
+						},
+						new RouteValueDictionary {
+							{"contentId", @"\d+" }
+						},
+						new RouteValueDictionary
+						{
+							{"area", "NogginBox.CustomFormsEdit"}
+						},
+						new MvcRouteHandler())
+				}
+			};
+		}
+	}
+}
