e7c6bb57bf146fad8808cfc1aa7862bf9c36e690
Richard Garside <729381+NogginBox@users.noreply.github.com>
2013-09-17 16:52:28 +0100

    Now checks content ownership if user only as EditOwnContent permission.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,11 +1,15 @@
 ﻿using System.Web.Mvc;
 using Orchard;
 using Orchard.ContentManagement;
+using Orchard.ContentManagement.Aspects;
 using Orchard.Localization;
 using Orchard.Security;
 using Orchard.Themes;
 using Orchard.UI.Notify;
 using OrchardContents = Orchard.Core.Contents;
+
+using Orchard.Security;
+//using Orchard.ContentPermissions.ViewModels;
 
 namespace NogginBox.CustomFormsEdit.Controllers
 {
@@ -13,13 +17,15 @@ namespace NogginBox.CustomFormsEdit.Controllers
 	public class HomeController : Controller, IUpdateModel
 	{
 		private readonly IAuthorizer _authorizer;
+		private readonly IAuthenticationService _authenticationService;
 		private readonly IOrchardServices _services;
 
 		public Localizer T { get; set; }
 
-		public HomeController(IAuthorizer authorizer, IOrchardServices services)
+		public HomeController(IAuthorizer authorizer, IAuthenticationService authenticationService, IOrchardServices services)
 		{
 			_authorizer = authorizer;
+			_authenticationService = authenticationService;
 			_services = services;
 		}
 
@@ -29,8 +35,11 @@ namespace NogginBox.CustomFormsEdit.Controllers
 			var content = _services.ContentManager.Get(contentId);
 			if (content == null) return HttpNotFound();
 
-			if(!_authorizer.Authorize(OrchardContents.Permissions.EditOwnContent, content, T("You don't have permission to edit this content")))
+			// Todo: Check for common part
+
+			if(!HasPermissionToEditThisContent(content))
 			{
+				_services.Notifier.Error(T("You don't have permission to edit this content."));
 				return View();
 			}
 
@@ -44,8 +53,9 @@ namespace NogginBox.CustomFormsEdit.Controllers
 			var content = _services.ContentManager.Get(contentId);
 			if (content == null) return HttpNotFound();
 
-			if(!_authorizer.Authorize(OrchardContents.Permissions.EditOwnContent, content, T("You don't have permission to edit this content")))
+			if(!HasPermissionToEditThisContent(content))
 			{
+				_services.Notifier.Error(T("You don't have permission to edit this content."));
 				// Todo: Redirect them back to actual content
 				return View();
 			}
@@ -61,6 +71,26 @@ namespace NogginBox.CustomFormsEdit.Controllers
 
 			// Todo: Redirect them back to actual content
             return RedirectToAction("Edit");
+        }
+
+		private bool HasPermissionToEditThisContent(IContent content)
+		{
+			return
+				_authorizer.Authorize(OrchardContents.Permissions.EditContent, content)
+				||
+				(_authorizer.Authorize(OrchardContents.Permissions.EditOwnContent, content) && HasOwnership(_services.WorkContext.CurrentUser, content));
+		}
+
+		private static bool HasOwnership(IUser user, IContent content)
+		{
+            if (user == null || content == null)
+                return false;
+
+            var common = content.As<ICommonPart>();
+            if (common == null || common.Owner == null)
+                return false;
+
+            return user.Id == common.Owner.Id;
         }
 
 		#region IUpdateModel Methods
