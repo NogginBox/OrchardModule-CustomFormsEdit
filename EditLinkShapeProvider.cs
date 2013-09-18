c7cbf44d2aa76dbdeb490f5343369f19616d4657
Richard Garside <729381+NogginBox@users.noreply.github.com>
2013-09-18 18:06:17 +0100

    Added edit wrapper

---
+++ EditLinkShapeProvider.cs
@@ -0,0 +1,15 @@
+﻿using Orchard.DisplayManagement.Descriptors;
+
+namespace NogginBox.CustomFormsEdit
+{
+	public class EditLinkShapeProvider : IShapeTableProvider
+	{
+		public void Discover(ShapeTableBuilder builder)
+		{
+			builder.Describe("Content")
+				.OnDisplaying(context => {
+					context.ShapeMetadata.Wrappers.Add("CustomFormsEditWrapper");
+				});
+		}
+	}
+}
