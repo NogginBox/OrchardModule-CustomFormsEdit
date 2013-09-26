bb343a49491f2af5252b7f29ded35d039adc4528
Richard Garside <729381+NogginBox@users.noreply.github.com>
2013-09-26 16:41:58 +0100

    Added CustomFormsEditLinkPart

---
+++ Drivers/CustomFormsEditLinkDriver.cs
@@ -0,0 +1,17 @@
+﻿using Orchard.ContentManagement;
+using Orchard.ContentManagement.Drivers;
+
+namespace NogginBox.CustomFormsEdit.Drivers
+{
+	public class CustomFormsEditLinkPart : ContentPart {}
+
+	public class ContentRatingDriver : ContentPartDriver<CustomFormsEditLinkPart>
+	{
+		protected override DriverResult Display(CustomFormsEditLinkPart part, string displayType, dynamic shapeHelper)
+		{
+			return ContentShape("Parts_CustomFormsEditLink",
+				() => shapeHelper.Parts_CustomFormsEditLink(
+					CotentId: 1));
+		}
+	}
+}
---
+++ Migration.cs
@@ -0,0 +1,16 @@
+﻿using Orchard.ContentManagement.MetaData;
+using Orchard.Core.Contents.Extensions;
+using Orchard.Data.Migration;
+
+namespace NogginBox.CustomFormsEdit
+{
+	public class Migration : DataMigrationImpl
+	{
+		public int Create() {
+			ContentDefinitionManager.AlterPartDefinition("CustomFormsEditLinkPart",
+				builder => builder.Attachable());
+
+			return 1;
+		}
+	}
+}
