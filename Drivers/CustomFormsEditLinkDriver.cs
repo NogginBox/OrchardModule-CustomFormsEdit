591ec6a12932d36c4ecc4f579790caed0bda5a35
Richard Garside <729381+NogginBox@users.noreply.github.com>
2013-09-26 17:06:52 +0100

    EditLinkPart driver now checks permissions.

--- Drivers/CustomFormsEditLinkDriver.cs
+++ Drivers/CustomFormsEditLinkDriver.cs
@@ -1,5 +1,7 @@
-﻿using Orchard.ContentManagement;
+﻿using Orchard;
+using Orchard.ContentManagement;
 using Orchard.ContentManagement.Drivers;
+using Orchard.Security;
 
 namespace NogginBox.CustomFormsEdit.Drivers
 {
@@ -7,8 +9,23 @@ namespace NogginBox.CustomFormsEdit.Drivers
 
 	public class ContentRatingDriver : ContentPartDriver<CustomFormsEditLinkPart>
 	{
+		private readonly CustomFormsEditService _customFormsEditService;
+		private readonly IOrchardServices _services;
+
+		public ContentRatingDriver(IAuthorizer authorizer, IOrchardServices services)
+		{
+			_services = services;
+			_customFormsEditService = new CustomFormsEditService(authorizer);
+		}
+
 		protected override DriverResult Display(CustomFormsEditLinkPart part, string displayType, dynamic shapeHelper)
 		{
+			var currentUser = _services.WorkContext.CurrentUser;
+			if (currentUser == null) return null;
+
+			// Check user has edit permission for content
+			if (!_customFormsEditService.UserHasPermissionToEditThisContent(part, currentUser)) return null;
+
 			return ContentShape("Parts_CustomFormsEditLink",
 				() => shapeHelper.Parts_CustomFormsEditLink(
 					CotentId: 1));
